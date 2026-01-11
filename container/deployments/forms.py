75a2e67589e1ac71728ca3cf1f1816f15dba86ef
Vicky Kumar <imvickykumar999@gmail.com>
2026-01-11 17:33:55 +0530

    container dropdown

--- container/deployments/forms.py
+++ container/deployments/forms.py
@@ -5,6 +5,22 @@ from .models import RailwayDeployment
 class RailwayDeploymentForm(forms.ModelForm):
     """Form for Railway deployment configuration."""
     
+    # Docker image choices
+    DOCKER_IMAGE_CHOICES = [
+        ('imvickykumar999/youtube-stream', 'imvickykumar999/youtube-stream (long lasting stream)'),
+        ('imvickykumar999/stream-downloader', 'imvickykumar999/stream-downloader (high quality stream)'),
+    ]
+    
+    # Override docker_image field to use dropdown
+    docker_image = forms.ChoiceField(
+        choices=DOCKER_IMAGE_CHOICES,
+        widget=forms.Select(attrs={
+            'class': 'form-control',
+        }),
+        help_text='Select a Docker image to deploy',
+        required=True
+    )
+    
     class Meta:
         model = RailwayDeployment
         fields = [
@@ -24,10 +40,6 @@ class RailwayDeploymentForm(forms.ModelForm):
                 'class': 'form-control',
                 'placeholder': 'Live Stream',
             }),
-            'docker_image': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'imvickykumar999/stream-downloader',
-            }),
             'stream_key': forms.TextInput(attrs={
                 'class': 'form-control',
                 'placeholder': 'Your YouTube stream key (optional)',
@@ -40,7 +52,6 @@ class RailwayDeploymentForm(forms.ModelForm):
         help_texts = {
             'railway_token': 'Get it from <a href="https://railway.com/account/tokens" target="_blank">Railway Account Tokens</a>',
             'project_name': 'Use a unique name for your project',
-            'docker_image': 'Docker image to deploy (e.g., imvickykumar999/stream-downloader)',
             'stream_key': 'Get it from <a href="https://studio.youtube.com/channel/UC/livestreaming" target="_blank">YouTube Studio Livestreaming</a>',
             'youtube_id': 'YouTube video ID (e.g., 4UTdZpN26Bs from https://www.youtube.com/watch?v=4UTdZpN26Bs)',
         }
@@ -50,5 +61,16 @@ class RailwayDeploymentForm(forms.ModelForm):
         # Make required fields more obvious
         self.fields['railway_token'].required = True
         self.fields['project_name'].required = True
-        self.fields['docker_image'].required = True
+        
+        # Set default value for docker_image if instance exists
+        if self.instance and self.instance.pk:
+            current_value = self.instance.docker_image
+            if current_value:
+                # If current value is not in choices, add it as an option
+                if current_value not in [choice[0] for choice in self.DOCKER_IMAGE_CHOICES]:
+                    self.fields['docker_image'].choices = [
+                        (current_value, f'{current_value} (current)'),
+                    ] + list(self.DOCKER_IMAGE_CHOICES)
+                # Set the initial value to the current value
+                self.fields['docker_image'].initial = current_value
 
